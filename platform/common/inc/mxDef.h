a6139dca3bde01b175b28405b728923927e8c737
niyoutian <niyoutian_0510@163.com>
2017-03-28 14:00:57 +0800

    2017-03-28 14:01

---
+++ platform/common/inc/mxDef.h
@@ -0,0 +1,16 @@
+#ifndef __MXDEF_H__
+#define __MXDEF_H__
+#include <stdlib.h>
+#include <errno.h>
+
+typedef char                    s8_t;
+typedef unsigned char           u8_t;
+typedef signed short            s16_t;
+typedef unsigned short          u16_t;
+typedef signed int              s32_t;
+typedef unsigned int            u32_t;
+typedef signed long long        s64_t;
+typedef unsigned long long      u64_t;
+
+
+#endif
