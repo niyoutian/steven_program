274ad06101d5cc8f5f3d5c6c42759dad2006e0aa
niyoutian <niyoutian_0510@163.com>
2017-03-30 19:55:12 +0800

    2017-03-30 19:55

---
+++ app/test_net/inc/test_net.h
@@ -0,0 +1,11 @@
+#ifndef __TEST_NET_H__
+#define __TEST_NET_H__
+
+
+
+
+
+
+
+
+#endif
---
+++ app/test_net/src/test_net.cpp
@@ -0,0 +1,20 @@
+#include "test_net.h"
+
+#include "mxLog.h"
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char* argv[])
+{
+	mxLogInit("Test");
+	mxLogFmt(LOG_DEBUG,"net test");
+	return 0;
+}
