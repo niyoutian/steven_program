61fb03e4e25f3d52acf2511d5e4b5a32b3d533af
niyoutian <niyoutian_0510@163.com>
2017-03-30 15:15:02 +0800

    2017-03-30 15:15

---
+++ platform/net/inc/mxEngineBase.h
@@ -0,0 +1,20 @@
+#ifndef __MX_ENGINE_BASE_H__
+#define __MX_ENGINE_BASE_H__
+
+
+class mxEngineBase
+{
+public:
+	mxEngineBase();
+	virtual ~mxEngineBase();
+private:
+};
+
+
+
+
+
+
+
+
+#endif
---
+++ platform/net/src/mxEngineBase.cpp
@@ -0,0 +1,14 @@
+#include "mxEngineBase.h"
+
+
+
+mxEngineBase::mxEngineBase()
+{
+
+	
+}
+
+mxEngineBase::~mxEngineBase()
+{
+	
+}
