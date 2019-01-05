64444979ebe5c84f8a96d1881f43d0c501cc42a0
niyoutian <niyoutian_0510@163.com>
2019-01-05 20:57:48 +0800

    add test

---
+++ test/test_hash/test_hash.cpp
@@ -0,0 +1,18 @@
+#include "stdio.h"
+#include "hashMd5.h"
+#include "test_log.h"
+#include "mxLog.h"
+
+
+int main(int argc, char* argv[])
+{
+	HashInterface *hash = new HashMd5();
+	if (hash == NULL)
+	{
+		printf("new HashMd5 error\n");
+	}
+	printf("hash type: %d\n",hash->getHashType());
+	printf("hash size: %d\n",hash->getHashSize());
+	
+	return 0;
+}
